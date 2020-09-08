4327e0c79d709de73b0159dba2679fb738792af8
samga <samgalaxy111111@gmail.com>
2020-09-09 06:21:59 +0700

    Toast & CountUp

--- app/src/main/java/id/sam/hellotoast/MainActivity.java
+++ app/src/main/java/id/sam/hellotoast/MainActivity.java
@@ -3,12 +3,30 @@ package id.sam.hellotoast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int mCount = 0;
+    private TextView txtCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtCount = findViewById(R.id.txtCount);
+    }
+
+    public void showToast(View view){
+        Toast.makeText(this,"Hello Toast", Toast.LENGTH_SHORT).show();
+    }
+
+    public void countUp(View view){
+        ++mCount;
+        if(txtCount != null){
+            txtCount.setText(Integer.toString(mCount));
+        }
     }
 }
